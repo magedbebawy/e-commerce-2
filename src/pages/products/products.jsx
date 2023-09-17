22a64a40e3c67a59bd4392bf5f06c83f5410d3e8
bebawymaged <102050541+bebawymaged@users.noreply.github.com>
2023-09-17 13:54:18 -0700

    add product api

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 function Products() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/addproduct');
+    }
     return(
         <div>
             Products
+            <button onClick={handleClick}>Add product</button>
         </div>
     )
 }
